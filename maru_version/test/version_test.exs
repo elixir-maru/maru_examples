efc3d6c2efb2b634b2ae34fa79170861cd2f1d1f
Paul Nicholson <paul@refactoredtech.com>
2017-03-08 00:56:02 -0500

    Add example test for mounted endpoints & versioning.
    This test is currently failing and I’m looking to understand how a test should be written to prove that the shared resource is available at v1. Also includes upgrading maru to 0.11.3 for this example.

--- maru_version/test/version_test.exs
+++ maru_version/test/version_test.exs
@@ -5,3 +5,16 @@ defmodule VersionTest do
     assert 1 + 1 == 2
   end
 end
+
+defmodule V1Test do
+  use ExUnit.Case
+  use Maru.Test, for: MaruVersion.API.V1
+
+  test "/" do
+    assert "It works V1!" = get("/", "v1") |> text_response
+  end
+
+  test "/shared" do
+    assert "It works for all version!" = get("/shared", "v1") |> text_response
+  end
+end
